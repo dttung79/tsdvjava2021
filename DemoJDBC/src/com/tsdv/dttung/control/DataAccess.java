b754e413721a10487a72a05a1eaed8ce80e65fd2
dttung79 <dttung@gmail.com>
2021-12-09 12:14:10 +0700

    Update DataAccess.java

--- DemoJDBC/src/com/tsdv/dttung/control/DataAccess.java
+++ DemoJDBC/src/com/tsdv/dttung/control/DataAccess.java
@@ -3,7 +3,8 @@ package com.tsdv.dttung.control;
 import java.sql.*;
 
 public class DataAccess {
-     protected Connection connectDB() throws SQLException {
+    protected  Connection conn;
+    protected Connection connectDB() throws SQLException {
         String url = "jdbc:mysql://localhost/tsdv";
         String user = "tsdv";
         String pass = "tsdv";
@@ -12,16 +13,16 @@ public class DataAccess {
     }
 
     protected ResultSet selectAll(String table) throws SQLException {
-        Connection conn = connectDB();
+        conn = connectDB();
         String sql = "SELECT * from " + table;
         Statement stm = conn.createStatement();
         ResultSet res = stm.executeQuery(sql);
-        conn.close();
+//        conn.close();
         return res;
     }
 
     protected boolean deleteByID(String table, String idCol, int id) throws SQLException {
-        Connection conn = connectDB();
+        conn = connectDB();
         String sql = "DELETE FROM " + table + " WHERE " + idCol + " = ?";
         PreparedStatement stm = conn.prepareStatement(sql);
         stm.setInt(1, id);
